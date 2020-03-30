7f2d1619cff4e7ea7f5ebb8fa3d73963ef76daa4
brenoafb <breno.afb@gmail.com>
2020-03-30 11:05:28 -0300

    Add instruction encoding for R, S, I types.

--- assembler.py
+++ assembler.py
@@ -40,15 +40,14 @@ instructions = {
         'ebreak'  : ('I', int('1110011', 2),   0x0)
 }
 
-def main():
-    i = input()
-    components = i.replace(',', '').split()
+def encode_instruction(instr):
+    components = get_components(instr)
     print(components)
     info = instructions[components[0]]
     typ = info[0]
     if typ == 'R':
         instr, rd, rs1, rs2 = components
-        _, opcode, f7, f3 = info
+        _, opcode, f3, f7 = info
         rd_i = int(rd[1:])
         rs1_i = int(rs1[1:])
         rs2_i = int(rs2[1:])
@@ -57,14 +56,37 @@ def main():
         print('rd: {}, rd_i = {}'.format(rd, rd_i))
         print('rs1: {}, rs1_i = {}'.format(rs1, rs1_i))
         print('rs2: {}, rs2_i = {}'.format(rs2, rs2_i))
-        print('f3: {0:b}'.format(f3))
-        print('f7: {0:b}'.format(f7))
+        print('f3: {:b}'.format(f3))
+        print('f7: {:b}'.format(f7))
 
-        print('{0:b}{0:b}{0:b}{0:b}{0:b}{0:b}'.format(opcode, rd_i, f3, rs1_i, rs2_i, f7))
+        return encode_rtype(opcode, rd_i, f3, rs1_i, rs2_i, f7)
+
     elif typ == 'I':
-        print('TODO')
+        instr, rd, rs1, imm = components
+        _, opcode, f3 = info
+        rd_i = int(rd[1:])
+        rs1_i = int(rs1[1:])
+        imm_b = twos_complement_str(int(imm), 12)
+        imm_i = int(imm)
+
+        print('instr: {}'.format(instr))
+        print('rd: {}, rd_i = {}'.format(rd, rd_i))
+        print('rs1: {}, rs1_i = {}'.format(rs1, rs1_i))
+        print('imm: {}'.format(imm))
+        print('f3: {0:b}'.format(f3))
+
+        return encode_itype(opcode, rd_i, f3, rs1_i, imm_i)
+
     elif typ == 'S':
-        print('TODO')
+        instr, rs1, imm, rs2 = components
+        _, opcode, f3 = info
+        rs1_i = int(rs1[1:])
+        rs2_i = int(rs2[1:])
+        imm_i = int(imm)
+
+
+        return encode_stype(opcode, f3, rs1_i, rs2_i, imm_i)
+
     elif typ == 'B':
         print('TODO')
     elif typ == 'J':
@@ -74,5 +96,47 @@ def main():
     else:
         print('ERROR: unsupported instruction type ' + typ)
 
+def twos_complement_str(x, width):
+    if x & (1 << (width - 1)) == 1:
+        return bin(x ^ ((1 << width) - 1) + 1)[2:]
+    return bin(x)[2:]
+
+def get_components(instr):
+    return instr.replace('(', ' ').replace(')', ' ').replace(',', '').split()
+
+def encode_rtype(opcode, rd, f3, rs1, rs2, f7):
+    return ((opcode & 0x7f)
+               | ((rd & 0x1f)  << 7)
+               | ((f3 & 0x7)   << 12)
+               | ((rs1 & 0x1f) << 15)
+               | ((rs2 & 0x1f) << 20)
+               | ((f7 & 0x7f)  << 25))
+
+
+def encode_itype(opcode, rd, f3, rs1, imm):
+   return ((opcode & 0x7f)
+              | ((rd & 0x1f)   << 7)
+              | ((f3 & 0x7)    << 12)
+              | ((rs1 & 0x1f)  << 15)
+              | ((imm & 0xfff) << 20))
+
+
+def encode_stype(opcode, f3, rs1, rs2, imm):
+    imm_lo5 = imm & 0x1f
+    imm_hi7 = (imm >> 5) & 0x7f
+    return ((opcode & 0x7f)
+               | ((imm_lo5 & 0x1f) << 7)
+               | ((f3 & 0x7) << 12)
+               | ((rs1 & 0x1f) << 15)
+               | ((rs2 & 0x1f) << 20)
+               | ((imm_hi7 & 0x7f) << 25)
+
+def main():
+    i = input()
+    encoded = encode_instruction(i)
+    print(hex(encoded))
+
 if __name__ == "__main__":
     main()
+
+
